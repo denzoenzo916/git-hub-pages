a20241dabbf9e395d8a51cde39b6a22ca40c79fa
denzoenzo916 <hoang.denny@gmail.com>
2025-05-10 16:35:35 -0700

    removed build folder

--- build/assets/js/0b393e2f.d7457aae.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkclassic=self.webpackChunkclassic||[]).push([[2413],{6835:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/git-hub-pages/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- build/assets/js/acecf23e.caa757c3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkclassic=self.webpackChunkclassic||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/git-hub-pages/blog","blogTitle":"Blog","authorsListPath":"/git-hub-pages/blog/authors"}')}}]);
